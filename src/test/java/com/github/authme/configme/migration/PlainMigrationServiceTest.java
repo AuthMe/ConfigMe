24f74600dd1867b3d061a57d3bb63a9512108be2
ljacqu <ljacqu@users.noreply.github.com>
2016-12-10 15:51:51 +0100

    Update Mockito version

--- src/test/java/com/github/authme/configme/migration/PlainMigrationServiceTest.java
+++ src/test/java/com/github/authme/configme/migration/PlainMigrationServiceTest.java
@@ -13,7 +13,7 @@ import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.mockito.Spy;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.io.File;
 import java.io.IOException;
