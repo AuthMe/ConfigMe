68bd8b597c5bae6cdd423de0d92b5dd2236c7a55
ljacqu <ljacqu@users.noreply.github.com>
2018-08-05 02:04:17 +0200

    More deleting

--- src/test/java/ch/jalu/configme/neo/properties/BeanPropertyTest.java
+++ src/test/java/ch/jalu/configme/neo/properties/BeanPropertyTest.java
@@ -1,20 +1,22 @@
-package ch.jalu.configme.properties;
+package ch.jalu.configme.neo.properties;
 
-import ch.jalu.configme.beanmapper.Mapper;
-import ch.jalu.configme.beanmapper.command.Command;
-import ch.jalu.configme.beanmapper.command.CommandConfig;
-import ch.jalu.configme.beanmapper.command.Executor;
-import ch.jalu.configme.beanmapper.worldgroup.WorldGroupConfig;
-import ch.jalu.configme.configurationdata.ConfigurationData;
-import ch.jalu.configme.resource.PropertyResource;
-import ch.jalu.configme.resource.YamlFileResource;
+import ch.jalu.configme.neo.beanmapper.Mapper;
+import ch.jalu.configme.neo.beanmapper.command.Command;
+import ch.jalu.configme.neo.beanmapper.command.CommandConfig;
+import ch.jalu.configme.neo.beanmapper.command.Executor;
+import ch.jalu.configme.neo.beanmapper.worldgroup.WorldGroupConfig;
+import ch.jalu.configme.neo.configurationdata.ConfigurationData;
+import ch.jalu.configme.neo.configurationdata.ConfigurationDataBuilder;
+import ch.jalu.configme.neo.resource.PropertyReader;
+import ch.jalu.configme.neo.resource.PropertyResource;
+import ch.jalu.configme.neo.resource.YamlFileResource;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
 
-import static ch.jalu.configme.TestUtils.copyFileFromResources;
+import static ch.jalu.configme.neo.TestUtils.copyFileFromResources;
 import static java.util.Collections.singletonList;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.equalTo;
@@ -24,7 +26,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 /**
- * Test for {@link BeanProperty} and its integration in {@link YamlFileResource}.
+ * Test for {@link BeanProperty}.
  */
 public class BeanPropertyTest {
 
@@ -38,13 +40,14 @@ public class BeanPropertyTest {
             new BeanProperty<>(CommandConfig.class, "commandconfig", new CommandConfig());
         File configFile = copyFileFromResources("/beanmapper/commands.yml", temporaryFolder);
         PropertyResource resource = new YamlFileResource(configFile);
+        ConfigurationData configurationData = ConfigurationDataBuilder.createConfiguration(singletonList(property));
+        configurationData.initializeValues(resource.createReader());
 
         // when
-        resource.exportProperties(new ConfigurationData(singletonList(property)));
-        resource = new YamlFileResource(configFile);
+        resource.exportProperties(configurationData);
 
         // then
-        CommandConfig config = property.getFromResource(resource);
+        CommandConfig config = property.getFromResource(resource.createReader());
         assertThat(config.getCommands().keySet(), contains("save", "refresh", "open"));
         Command refreshCommand = config.getCommands().get("refresh");
         assertThat(refreshCommand.getExecution().getPrivileges(), contains("page.view", "action.refresh"));
@@ -63,13 +66,14 @@ public class BeanPropertyTest {
             new BeanProperty<>(CommandConfig.class, "", new CommandConfig());
         File configFile = copyFileFromResources("/beanmapper/commands_root_path.yml", temporaryFolder);
         PropertyResource resource = new YamlFileResource(configFile);
+        ConfigurationData configurationData = ConfigurationDataBuilder.createConfiguration(singletonList(property));
+        configurationData.initializeValues(resource.createReader());
 
         // when
-        resource.exportProperties(new ConfigurationData(singletonList(property)));
-        resource = new YamlFileResource(configFile);
+        resource.exportProperties(configurationData);
 
         // then
-        CommandConfig config = property.getFromResource(resource);
+        CommandConfig config = property.getFromResource(resource.createReader());
         assertThat(config.getCommands().keySet(), contains("save"));
         Command saveCommand = config.getCommands().get("save");
         assertThat(saveCommand.getExecution().getPrivileges(), contains("action.open", "action.save"));
@@ -83,17 +87,17 @@ public class BeanPropertyTest {
         String path = "cnf";
         BeanProperty<WorldGroupConfig> property =
             new BeanProperty<>(WorldGroupConfig.class, path, new WorldGroupConfig(), mapper);
-        PropertyResource resource = mock(PropertyResource.class);
+        PropertyReader reader = mock(PropertyReader.class);
         Object value = new Object();
-        given(resource.getObject(path)).willReturn(value);
+        given(reader.getObject(path)).willReturn(value);
         WorldGroupConfig groupConfig = new WorldGroupConfig();
-        given(mapper.convertToBean(path, resource, WorldGroupConfig.class)).willReturn(groupConfig);
+        given(mapper.convertToBean(reader, path, WorldGroupConfig.class)).willReturn(groupConfig);
 
         // when
-        WorldGroupConfig result = property.getValue(resource);
+        WorldGroupConfig result = property.determineValue(reader);
 
         // then
         assertThat(result, equalTo(groupConfig));
-        verify(mapper).convertToBean(path, resource, WorldGroupConfig.class);
+        verify(mapper).convertToBean(reader, path, WorldGroupConfig.class);
     }
 }
