d8b5a64534e65432dbd10040c0539a582fc3e534
ljacqu <ljacqu@users.noreply.github.com>
2019-02-27 20:04:41 +0100

    #77 Add test to ensure that numbers in the path are supported

---
+++ src/test/java/ch/jalu/configme/resource/YamlFileReaderPathsWithNumberTest.java
@@ -0,0 +1,71 @@
+package ch.jalu.configme.resource;
+
+import ch.jalu.configme.SettingsHolder;
+import ch.jalu.configme.TestUtils;
+import ch.jalu.configme.properties.Property;
+import ch.jalu.configme.samples.TestEnum;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+
+import static ch.jalu.configme.properties.PropertyInitializer.newProperty;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Tests that paths which have a number in them can be read properly.
+ *
+ * @see <a href="https://github.com/AuthMe/ConfigMe/issues/77">Issue #77</a>
+ */
+public class YamlFileReaderPathsWithNumberTest {
+
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+    @Test
+    public void shouldReadPropertiesFromFile() {
+        // given
+        File file = TestUtils.copyFileFromResources("/configurationfiles/config-with-number-paths.yml", temporaryFolder);
+        YamlFileResource yamlFileResource = new YamlFileResource(file);
+        PropertyReader reader = yamlFileResource.createReader();
+
+        // when / then
+        assertThat(PathsWithNumbers.ONE_LEVEL.determineValue(reader), equalTo(1));
+        assertThat(PathsWithNumbers.TWO_LEVEL.determineValue(reader), equalTo(2));
+        assertThat(PathsWithNumbers.ROLE_ZERO_NAME.determineValue(reader), equalTo("Initial role"));
+        assertThat(PathsWithNumbers.ROLE_ZERO_CHANGE_HOME.determineValue(reader), equalTo(false));
+        assertThat(PathsWithNumbers.ROLE_ONE_NAME.determineValue(reader), equalTo("Expert role"));
+        assertThat(PathsWithNumbers.ROLE_ONE_CHANGE_HOME.determineValue(reader), equalTo(true));
+        assertThat(PathsWithNumbers.ROLE_FALSE_ENUM.determineValue(reader), equalTo(TestEnum.SECOND));
+    }
+
+    /** Test configuration. */
+    public static final class PathsWithNumbers implements SettingsHolder {
+
+        public static final Property<Integer> ONE_LEVEL =
+            newProperty("tiers.list.1.level", 999);
+
+        public static final Property<Integer> TWO_LEVEL =
+            newProperty("tiers.list.2.level", 999);
+
+        public static final Property<String> ROLE_ZERO_NAME =
+            newProperty("roles.0.name", "");
+
+        public static final Property<Boolean> ROLE_ZERO_CHANGE_HOME =
+            newProperty("roles.0.change-home", false);
+
+        public static final Property<String> ROLE_ONE_NAME =
+            newProperty("roles.1.name", "");
+
+        public static final Property<Boolean> ROLE_ONE_CHANGE_HOME =
+            newProperty("roles.1.change-home", false);
+
+        public static final Property<TestEnum> ROLE_FALSE_ENUM =
+            newProperty(TestEnum.class, "roles.false.-5", TestEnum.FOURTH);
+
+        private PathsWithNumbers() {
+        }
+    }
+}
