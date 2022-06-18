9d682ccabe9432b9402fbfd7c1d1c9e048740397
zrdzn <zrdzn9@gmail.com>
2022-06-18 15:46:29 +0200

    Add a missing comment

--- src/main/java/ch/jalu/configme/properties/ShortProperty.java
+++ src/main/java/ch/jalu/configme/properties/ShortProperty.java
@@ -3,6 +3,9 @@ package ch.jalu.configme.properties;
 import ch.jalu.configme.properties.types.PrimitivePropertyType;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Short property. This extension exists for convenience and backwards compatibility.
+ */
 public class ShortProperty extends TypeBasedProperty<Short> {
 
     public ShortProperty(@NotNull String path, @NotNull Short defaultValue) {
