e9c33d43d1cc6c0b509e040b527918fb9ef1e1d9
lengeligor <lengel.igor@gmail.com>
2024-10-07 22:23:40 +0200

    383 -> edit by review comments

--- src/main/java/ch/jalu/configme/properties/EnumSetProperty.java
+++ src/main/java/ch/jalu/configme/properties/EnumSetProperty.java
@@ -3,7 +3,9 @@ package ch.jalu.configme.properties;
 import ch.jalu.configme.properties.types.EnumSetPropertyType;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
 import java.util.EnumSet;
+import java.util.Set;
 
 /**
  * EnumSet property.
@@ -17,6 +19,13 @@ public class EnumSetProperty<E extends Enum<E>> extends SetProperty<E> {
     }
 
     public EnumSetProperty(@NotNull String path, @NotNull Class<E> enumClass, @NotNull E @NotNull... defaultValue) {
-        super(path, new EnumSetPropertyType(enumClass), defaultValue);
+        super(new EnumSetPropertyType(enumClass), path, newEnumSet(enumClass, defaultValue));
+    }
+
+    private static <E extends Enum<E>> @NotNull Set<E> newEnumSet(@NotNull Class<E> enumClass,
+                                                                  E @NotNull [] defaultValue) {
+        EnumSet<E> enumSet = EnumSet.noneOf(enumClass);
+        enumSet.addAll(Arrays.asList(defaultValue));
+        return enumSet;
     }
 }
