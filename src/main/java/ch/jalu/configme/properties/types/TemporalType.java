8d789bb9a2b41f4bf67436c051ecc266df1d2f05
Dhruv Joshi <thepoetdj@gmail.com>
2025-12-12 00:14:56 +0530

    #481 Enhance TemporalType with matching export format along with additional LocalTime/LocalDateTime types

--- src/main/java/ch/jalu/configme/properties/types/TemporalType.java
+++ src/main/java/ch/jalu/configme/properties/types/TemporalType.java
@@ -5,6 +5,8 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.time.temporal.Temporal;
@@ -22,15 +24,34 @@ public class TemporalType<T extends Temporal> extends PropertyAndLeafType<T> {
     /** Local Date temporal type. */
     public static final TemporalType<LocalDate> LOCAL_DATE = new TemporalType<>(
         LocalDate.class, Arrays.asList("yyyy-MM-dd", "dd.MM.yyyy", "MM/dd/yyyy"), LocalDate::parse);
+    /** Local Time temporal type. */
+    public static final TemporalType<LocalTime> LOCAL_TIME = new TemporalType<>(
+        LocalTime.class, Arrays.asList("HH:mm:ss", "HH.mm", "HH:mm"), LocalTime::parse);
+    /** Local Date Time temporal type. */
+    public static final TemporalType<LocalDateTime> LOCAL_DATE_TIME = new TemporalType<>(
+        LocalDateTime.class, Arrays.asList("yyyy-MM-dd HH:mm:ss", "dd.MM.yyyy HH:mm:ss", "MM/dd/yyyy HH:mm:ss"),
+        LocalDateTime::parse);
 
     private final List<String> supportedFormats;
     private final BiFunction<String, DateTimeFormatter, T> temporalParser;
+    private String defaultExportFormat;
 
-    protected TemporalType(Class<T> clazz, List<String> supportedFormats,
-                           BiFunction<String, DateTimeFormatter, T> defaultParser) {
+    /**
+     * Constructor.
+     *
+     * @param clazz the temporal type this type should convert to
+     * @param supportedFormats list of conversion formats supported for this type
+     * @param defaultParser function which can parse a value to this type in one of the given supportedFormats
+     */
+    public TemporalType(@NotNull Class<T> clazz, @NotNull List<String> supportedFormats,
+                        @NotNull BiFunction<String, DateTimeFormatter, T> defaultParser) {
         super(clazz);
+        if (supportedFormats.isEmpty()) {
+            throw new IllegalArgumentException("At least one supported format must be provided.");
+        }
         this.supportedFormats = supportedFormats;
         this.temporalParser = defaultParser;
+        this.defaultExportFormat = supportedFormats.get(0);
     }
 
     @Override
@@ -43,16 +64,15 @@ public class TemporalType<T extends Temporal> extends PropertyAndLeafType<T> {
 
     @Override
     public @Nullable Object toExportValue(@NotNull T value) {
-        if (value instanceof LocalDate) {
-            return LocalDate.from(value).toString();
-        }
-        return value.toString();
+        return DateTimeFormatter.ofPattern(this.defaultExportFormat).format(value);
     }
 
     private T convertToTemporalType(String temporalText) {
         for (String format: this.supportedFormats) {
             try {
-                return this.temporalParser.apply(temporalText, DateTimeFormatter.ofPattern(format));
+                T parsedValue = this.temporalParser.apply(temporalText, DateTimeFormatter.ofPattern(format));
+                this.defaultExportFormat = format;
+                return parsedValue;
             } catch (DateTimeParseException e) {
                 // try next format
             }
--- src/test/java/ch/jalu/configme/properties/types/TemporalTypeTest.java
+++ src/test/java/ch/jalu/configme/properties/types/TemporalTypeTest.java
@@ -4,11 +4,14 @@ import ch.jalu.configme.properties.convertresult.ConvertErrorRecorder;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Collections;
 
 import static ch.jalu.typeresolver.TypeInfo.of;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Test for {@link TemporalType}.
@@ -16,15 +19,34 @@ import static org.hamcrest.Matchers.nullValue;
 public class TemporalTypeTest {
 
     @Test
+    void shouldNotAllowToInstantiateTemporalTypeWithInvalidArguments() {
+        // when
+        IllegalArgumentException noSupportedFormats = assertThrows(IllegalArgumentException.class,
+            () -> new TemporalType<>(LocalDate.class, Collections.emptyList(), LocalDate::parse));
+
+        // then
+        assertThat(noSupportedFormats.getMessage(), matchesPattern("At least one supported format must be provided."));
+    }
+
+    @Test
     void shouldConvertToGivenTemporalTypeForSupportedFormat() {
         // given
         ConvertErrorRecorder errorRecorder = new ConvertErrorRecorder();
         LocalDate localDate = LocalDate.of(1970, 1, 31);
+        LocalTime localTimeFull = LocalTime.of(15, 35, 40);
+        LocalTime localTimeShort = LocalTime.of(15, 35);
+        LocalDateTime localDateTime = LocalDateTime.of(1970, 1, 31, 15, 35, 40);
 
         // when / then
         assertThat(TemporalType.LOCAL_DATE.convert("1970-01-31", of(LocalDate.class), errorRecorder), equalTo(localDate));
         assertThat(TemporalType.LOCAL_DATE.convert("31.01.1970", of(LocalDate.class), errorRecorder), equalTo(localDate));
         assertThat(TemporalType.LOCAL_DATE.convert("01/31/1970", of(LocalDate.class), errorRecorder), equalTo(localDate));
+        assertThat(TemporalType.LOCAL_TIME.convert("15:35:40", of(LocalTime.class), errorRecorder), equalTo(localTimeFull));
+        assertThat(TemporalType.LOCAL_TIME.convert("15.35", of(LocalTime.class), errorRecorder), equalTo(localTimeShort));
+        assertThat(TemporalType.LOCAL_TIME.convert("15:35", of(LocalTime.class), errorRecorder), equalTo(localTimeShort));
+        assertThat(TemporalType.LOCAL_DATE_TIME.convert("1970-01-31 15:35:40", of(LocalDateTime.class), errorRecorder), equalTo(localDateTime));
+        assertThat(TemporalType.LOCAL_DATE_TIME.convert("31.01.1970 15:35:40", of(LocalDateTime.class), errorRecorder), equalTo(localDateTime));
+        assertThat(TemporalType.LOCAL_DATE_TIME.convert("01/31/1970 15:35:40", of(LocalDateTime.class), errorRecorder), equalTo(localDateTime));
     }
 
     @Test
@@ -36,6 +58,13 @@ public class TemporalTypeTest {
         assertThat(TemporalType.LOCAL_DATE.convert("31-01-1970", of(LocalDate.class), errorRecorder), nullValue());
         assertThat(TemporalType.LOCAL_DATE.convert("1970.01.31", of(LocalDate.class), errorRecorder), nullValue());
         assertThat(TemporalType.LOCAL_DATE.convert("1970/01/31", of(LocalDate.class), errorRecorder), nullValue());
+        assertThat(TemporalType.LOCAL_TIME.convert("25:35:40", of(LocalTime.class), errorRecorder), nullValue());
+        assertThat(TemporalType.LOCAL_TIME.convert("15.35.40", of(LocalTime.class), errorRecorder), nullValue());
+        assertThat(TemporalType.LOCAL_TIME.convert("15:35.40", of(LocalTime.class), errorRecorder), nullValue());
+        assertThat(TemporalType.LOCAL_TIME.convert("15.35:40", of(LocalTime.class), errorRecorder), nullValue());
+        assertThat(TemporalType.LOCAL_DATE_TIME.convert("31-01-1970 25:35:40", of(LocalDateTime.class), errorRecorder), nullValue());
+        assertThat(TemporalType.LOCAL_DATE_TIME.convert("1970.01.31 15.35.40", of(LocalDateTime.class), errorRecorder), nullValue());
+        assertThat(TemporalType.LOCAL_DATE_TIME.convert("1970/01/31 15.35:40", of(LocalDateTime.class), errorRecorder), nullValue());
     }
 
     @Test
@@ -45,6 +74,8 @@ public class TemporalTypeTest {
 
         // when / then
         assertThat(TemporalType.LOCAL_DATE.convert("test", of(LocalDate.class), errorRecorder), nullValue());
+        assertThat(TemporalType.LOCAL_TIME.convert("test", of(LocalTime.class), errorRecorder), nullValue());
+        assertThat(TemporalType.LOCAL_DATE_TIME.convert("test", of(LocalDateTime.class), errorRecorder), nullValue());
     }
 
     @Test
@@ -54,11 +85,32 @@ public class TemporalTypeTest {
 
         // when / then
         assertThat(TemporalType.LOCAL_DATE.convert(null, of(LocalDate.class), errorRecorder), nullValue());
+        assertThat(TemporalType.LOCAL_TIME.convert(null, of(LocalTime.class), errorRecorder), nullValue());
+        assertThat(TemporalType.LOCAL_DATE_TIME.convert(null, of(LocalDateTime.class), errorRecorder), nullValue());
     }
 
     @Test
     void shouldExportValueAsString() {
         // given / when / then
         assertThat(TemporalType.LOCAL_DATE.toExportValue(LocalDate.of(1970, 1, 31)), equalTo("1970-01-31"));
+        assertThat(TemporalType.LOCAL_TIME.toExportValue(LocalTime.of(13, 55, 13)), equalTo("13:55:13"));
+        assertThat(TemporalType.LOCAL_DATE_TIME.toExportValue(LocalDateTime.of(1970, 1, 31, 12, 30, 47)), equalTo("1970-01-31 12:30:47"));
+    }
+
+    @Test
+    void shouldExportValueInMatchingFormatAsString() {
+        // given
+        ConvertErrorRecorder errorRecorder = new ConvertErrorRecorder();
+        LocalDate localDate = TemporalType.LOCAL_DATE.convert("01/31/1970", errorRecorder);
+        LocalTime localTime = TemporalType.LOCAL_TIME.convert("11:11", errorRecorder);
+        LocalDateTime localDateTime = TemporalType.LOCAL_DATE_TIME.convert("31.01.1970 13:55:13", errorRecorder);
+
+        // when / then
+        assertThat(localDate, notNullValue());
+        assertThat(localTime, notNullValue());
+        assertThat(localDateTime, notNullValue());
+        assertThat(TemporalType.LOCAL_DATE.toExportValue(localDate), equalTo("01/31/1970"));
+        assertThat(TemporalType.LOCAL_TIME.toExportValue(localTime), equalTo("11:11"));
+        assertThat(TemporalType.LOCAL_DATE_TIME.toExportValue(localDateTime), equalTo("31.01.1970 13:55:13"));
     }
 }
